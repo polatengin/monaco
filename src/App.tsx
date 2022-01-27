22418482b3295fecc895d46671d751e7edb36828
polatengin <enpolat@microsoft.com>
2022-01-27 13:43:03 -0800

    adding user type

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React, { FunctionComponent } from 'react';
 
+type User = {
+  name: string;
+  pinnedPages: number[];
+  pagesOrder: number[];
+};
+
 export const App: FunctionComponent = () => {
   return (
     <div>
